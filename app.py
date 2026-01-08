1012bee93681f23231424b50c803ee3263ccddad
RAJA240104015 <240104015@mhs.udb.ac.id>
2026-01-08 10:14:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,12 @@
-# cloudbuild.yaml
-options:
-  logging: CLOUD_LOGGING_ONLY
-  substitutionOption: ALLOW_LOOSE
+from flask import Flask
+import os
 
-substitutions:
-  _REGION: 'us-central1'
+app = Flask(__name__)
 
-steps:
-  # Step 1: Memulai Build Docker Image
-  - id: 'Start Build'
-    name: 'gcr.io/cloud-builders/docker'
-    args:
-      [
-        'build',
-        '-t', 'gcr.io/$PROJECT_ID/cloud-run-app:${BRANCH_NAME}-${SHORT_SHA}',
-        '.'
-      ]
-    waitFor: ['-']
-    entrypoint: 'bash'
-    env:
-      - 'BRANCH_NAME=$BRANCH_NAME'
-      - 'SHORT_SHA=$SHORT_SHA'
+@app.route("/")
+def hello():
+    return "Cloud Run OK 🚀"
 
-  # Step 2: Push Docker Image ke Google Container Registry
-  - id: 'Push Image'
-    name: 'gcr.io/cloud-builders/docker'
-    args:
-      [
-        'push',
-        'gcr.io/$PROJECT_ID/cloud-run-app:${BRANCH_NAME}-${SHORT_SHA}'
-      ]
-    waitFor: ['Start Build']
-
-  # Step 3: Deploy ke Cloud Run
-  - id: 'Deploy Cloud Run'
-    name: 'gcr.io/google.com/cloudsdktool/cloud-sdk'
-    entrypoint: 'gcloud'
-    args:
-      [
-        'run', 'deploy', 'cloud-run-app',
-        '--image', 'gcr.io/$PROJECT_ID/cloud-run-app:${BRANCH_NAME}-${SHORT_SHA}',
-        '--region', '$_REGION',
-        '--platform', 'managed',
-        '--allow-unauthenticated'
-      ]
-    waitFor: ['Push Image']
-
-images:
-  - 'gcr.io/$PROJECT_ID/cloud-run-app:${BRANCH_NAME}-${SHORT_SHA}'
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
